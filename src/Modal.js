c3215036ef486402e97cd7ee2232c7a397fe3d48
PJMantoss <joelptoss@gmail.com>
2022-08-11 13:25:21 +0100

    update App

--- src/Modal.js
+++ src/Modal.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Modal = ({open}) => {
   return (
     <div className='overlay'>
-        <div className='modalContainer'></div>
+        <div className='modalContainer'>
+            
+        </div>
     </div>
   )
 }
