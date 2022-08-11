1d5ce3c7e11a164f81f123662e6ef28c8e76dcf2
PJMantoss <joelptoss@gmail.com>
2022-08-11 13:29:01 +0100

    update Modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import mickey from './mickey.jpg';
 
 const Modal = ({open}) => {
   return (
     <div className='overlay'>
         <div className='modalContainer'>
-            <img src='' alt='' />
+            <img src={mickey} alt='' />
         </div>
     </div>
   )
